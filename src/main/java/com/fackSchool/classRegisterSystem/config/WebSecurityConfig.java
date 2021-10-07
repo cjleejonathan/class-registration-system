41b7af1e40b45eff537754dba1f9a974bab89e27
Jonathan Lee <j0975514018@gmail.com>
2021-10-06 20:06:26 -0700

    header background

--- src/main/java/com/fackSchool/classRegisterSystem/config/WebSecurityConfig.java
+++ src/main/java/com/fackSchool/classRegisterSystem/config/WebSecurityConfig.java
@@ -48,8 +48,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-    	http.formLogin()
-    	  .failureUrl("/student-form?error=true");
+    	http
+        .authorizeRequests()
+            .anyRequest().authenticated() 
+            .and()
+        .formLogin()                      
+            .and()
+        .httpBasic();                 
     }
 
 	
